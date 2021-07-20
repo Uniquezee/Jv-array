c45d63d4643f501b807d4c4529cb0e434642c569
uniquezee <priscaonyemaobiamarachi@gmail.com>
2021-07-20 17:08:55 +0100

    js-array

---
+++ index.js
@@ -0,0 +1,72 @@
+/*ASsignment
+Question 2:Multi dimensional Arrays
+var arr = [[1, 2, 3],
+           [4, 5, 6],7,8,9,
+           [[10, 11, 12, [13, 4, 112]], 136, 14],];
+ *print out these numbers (10,8,13,136 and 4)from the array provided above*/
+  /*         var arr = [
+               [1 ,2, 3],
+               [4, 5, 6], 7, 8, 9,
+               [[10,11,12,[13,4,112]], 136,14,]
+       ];
+for (let i = 0; i < arr.length; i++){
+     var arr = arr[i];
+     for( let j = 0; j < arr.length; j++){
+         console.log("arr[" + i + "] ["+ j + "] = " + arr[j]);
+     }
+}
+
+var arr = [
+    [1 ,2, 3],
+    [4, 5, 6], 7, 8, 9,
+    [[10,11,12,[13,4,112]], 136,14,]
+];
+ for (let i = 0; i < arr.length; i++){
+var arr = arr[i];
+for( let j = 0; j < arr.length; j++){
+console.log("arr[" + i + "] ["+ j + "] = " + arr[j]);
+}
+}*/
+ /*for(let i = 0; i < arr.length; i++){
+     var arr = arr[i];
+     console.log(i)
+     /*for(j=0; j<arr[i].length; j++){
+         console.log(j)
+     }
+     for(let j = 0; j<arr.length; j++){
+         console.log(j)
+     }
+ }*/
+
+ var arr = [
+    [1 ,2, 3],
+    [4, 5, 6], 7, 8, 9,
+    [[10,11,12,[13,4,112]], 136,14,]
+];
+for (let i = 0; i < arr.length; i++){
+    for(j=0; j<arr[i].length; j++){
+        console.log(arr[i][j])
+    }
+}
+
+var arr = [
+    [1 ,2, 3],
+    [4, 5, 6], 7, 8, 9,
+    [[10,11,12,[13,4,112]], 136,14,]
+];
+for (let i = 0; i < arr.length; i++){
+    for(j=0; j<arr[i].length; j++){
+        console.log(arr[i][j])
+        }
+}
+
+var arr = [
+    [1 ,2, 3],
+    [4, 5, 6], 7, 8, 9,
+    [[10,11,12,[13,4,112]], 136,14,]
+];
+for (let i = 0; i < arr.length; i++){
+    for(j=0; j<arr[i].length; j++){
+        console.log(arr[i][j][0])
+        }
+}
