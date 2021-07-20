65fd910ada8364fefbbf40328054e4dd51951ab3
uniquezee <priscaonyemaobiamarachi@gmail.com>
2021-07-20 17:22:37 +0100

    js-array

--- index.js
+++ index.js
@@ -36,7 +36,7 @@ console.log("arr[" + i + "] ["+ j + "] = " + arr[j]);
      for(let j = 0; j<arr.length; j++){
          console.log(j)
      }
- }*/
+ }
 
  var arr = [
     [1 ,2, 3],
@@ -69,4 +69,22 @@ for (let i = 0; i < arr.length; i++){
     for(j=0; j<arr[i].length; j++){
         console.log(arr[i][j][0])
         }
+}*/
+
+var arr = [
+    [1 ,2, 3],
+    [4, 5, 6], 7, 8, 9,
+    [[10,11,12,[13,4,112]], 136,14,]
+];
+for (let i = 0; i < arr.length; i++){
+    let result = arr[i];
+    if(result === arr[5]){
+        console.log(result[0][0]);
+        console.log(result[0][3][0]);
+        console.log(result[1]);
+    }else if(result === arr[3]){
+        console.log(result);
+    }else if(result === arr[1]){
+        console.log(result[0]);
+    }
 }
